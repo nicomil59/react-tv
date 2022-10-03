432a4fb58a6bd5853c953d945590fe2258a08096
Nicolas Milhem <nicolasmilhem@gmail.com>
2022-10-03 17:36:18 +0200

    gestion des genres dans Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from "axios";
 
 const Card = ( {show }) => {
+  
+  // const genres_ids = show.genre_ids;
+  // console.log(genres_ids);
+
+  const [genres, setGenres] = useState([]);
+
+  useEffect(() => {
+    axios.get(`https://api.themoviedb.org/3/genre/tv/list?api_key=ee5257db9bf57231392a184bbd8e9562&language=fr-FR`)
+    .then((res) => {
+      const listGenres = res.data.genres;
+      // console.log(listGenres);
+      // console.log(listGenres.filter(item => show.genre_ids.includes(item.id)));
+      setGenres(listGenres.filter(item => show.genre_ids.includes(item.id)).map(genre => genre.name));
+    })
+  }, [])
+  
+  
   return (
     <li className='card' style={{marginBottom: "30px", background: "#F7F7F7"}}>
         <img style={{maxWidth: "150px", height: "auto"}} className='card-img' src={show.poster_path ? `https://image.tmdb.org/t/p/original${show.poster_path}` : './img/poster.jpg'} alt={`affiche de ${show.name}`} />
@@ -8,7 +26,9 @@ const Card = ( {show }) => {
             <h2 className="card-title">{show.name}</h2>
             <p className='card-date'>Sortie le {show.first_air_date}</p>
             <p className='card-rating'>{show.vote_average}/10 ⭐️</p>
-            <p>Genres</p>
+            <ul>
+              {genres && genres.map((genre, index) => <li key={index}>{genre}</li>)}
+            </ul>
             <h3>Synopsis</h3>
             <p className="card-synopsis">{show.overview}</p>
         </div>
