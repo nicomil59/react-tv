e4febb9bf86ab76cb94804e2b722cbc3c6c82b7c
Nicolas Milhem <nicolasmilhem@gmail.com>
2022-10-03 15:57:52 +0200

    ajout boutons top-flop

--- src/components/Series.js
+++ src/components/Series.js
@@ -7,30 +7,50 @@ const Series = () => {
   const [series, setSeries] = useState([]);
   const [error, setError] = useState(null);
   const [search, setSearch] = useState('');
+  const [sortOrder, setSortOrder] = useState(null);
 
   const url = search === '' ? 
     "https://api.themoviedb.org/3/tv/popular?api_key=ee5257db9bf57231392a184bbd8e9562&language=fr-FR&page=1"
     :
     `https://api.themoviedb.org/3/search/tv?api_key=ee5257db9bf57231392a184bbd8e9562&query=${search}&language=fr-FR`;
 
+  const sortSeries = (series, sortOrder) => {
+    if (sortOrder === 'top') {
+      return series.sort((a, b) => b.vote_average - a.vote_average)
+    } else if (sortOrder === 'flop') {
+      return series.sort((a, b) => a.vote_average - b.vote_average)
+    }
+  }
+
   useEffect(() => {
     axios.get(url)
-      .then((res) => setSeries(res.data.results))
+      .then((res) => {
+        if(sortOrder === null) {
+          setSeries(res.data.results);
+        } else {
+          setSeries(sortSeries(res.data.results, sortOrder));
+        }
+      })
       .catch(error => {
         setError(error);
       })
-  }, [url]);
+  }, [url, sortOrder]);
 
   if (error) return `Error: ${error.message}`;
 
   const handleSearch = term => {
     console.log("term", term);
     setSearch(term);
+    setSortOrder(null);
   }
 
   return (
     <div>
       <Search placeholder="Entrez une série" getSearch={handleSearch} />
+      <div className="buttons-sort">
+        <button onClick={() => setSortOrder('top')} className="btn btn-top">Top</button>
+        <button onClick={() => setSortOrder('flop')} className="btn btn-flop">Flop</button>
+      </div>
       <ul style={{ listStyleType: "none" }}>
         {series.length > 0 ? series.map((item) => <Card key={item.id} show={item} />) : <li>Pas de données !</li>}
       </ul>
