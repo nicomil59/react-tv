58d50b342b77f6405fa9ac554be3802e4a154d9d
Nicolas Milhem <nicolasmilhem@gmail.com>
2022-10-04 11:35:44 +0200

    gestion de la date dans Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import axios from "axios";
+import moment from 'moment';
+import 'moment/locale/fr';
 
 const Card = ( {show }) => {
   
-  // const genres_ids = show.genre_ids;
+  const genres_ids = show.genre_ids;
   // console.log(genres_ids);
 
   const [genres, setGenres] = useState([]);
@@ -14,17 +16,20 @@ const Card = ( {show }) => {
       const listGenres = res.data.genres;
       // console.log(listGenres);
       // console.log(listGenres.filter(item => show.genre_ids.includes(item.id)));
-      setGenres(listGenres.filter(item => show.genre_ids.includes(item.id)).map(genre => genre.name));
+      setGenres(listGenres.filter(item => genres_ids.includes(item.id)).map(genre => genre.name));
     })
-  }, [])
-  
+  }, [genres_ids])
+
+  const getTime = time => {
+    return moment(time).format('L');
+  }
   
   return (
     <li className='card' style={{marginBottom: "30px", background: "#F7F7F7"}}>
         <img style={{maxWidth: "150px", height: "auto"}} className='card-img' src={show.poster_path ? `https://image.tmdb.org/t/p/original${show.poster_path}` : './img/poster.jpg'} alt={`affiche de ${show.name}`} />
         <div className="card-content">
             <h2 className="card-title">{show.name}</h2>
-            <p className='card-date'>Sortie le {show.first_air_date}</p>
+            <p className='card-date'>Sortie le {getTime(show.first_air_date)}</p>
             <p className='card-rating'>{show.vote_average}/10 ⭐️</p>
             <ul>
               {genres && genres.map((genre, index) => <li key={index}>{genre}</li>)}
