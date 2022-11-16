0b7ef584c97961b5f18285e40c1b12a3e81c3afe
Nicolas Milhem <nicolasmilhem@gmail.com>
2022-11-16 15:25:07 +0100

    modif suppr path NotFound

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/bookmarks" element={<Bookmarks />} />
-          <Route path="/*" element={<NotFound />} />
+          {/* <Route path="*" element={<NotFound />} /> */}
         </Routes>
       </BrowserRouter>
     </GenresContextProvider>
